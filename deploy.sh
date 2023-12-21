70fb45c4007e14678c6be6e95bd38229d17f7c51
Fabio <fabiodsilva@yahoo.com.br>
2023-12-21 15:45:12 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
+
 python3 -m pip install simple-term-menu
 wget https://raw.githubusercontent.com/aws-samples/aws-organizations-alternate-contact-manager/main/script.py
 python3 script.py
